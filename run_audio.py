b006c15625bb6da58e596dfd4923af47af1dfd8a
laokashouji <fantesticday@gmail.com>
2023-06-07 16:41:43 +0800

    update

--- run_audio.py
+++ run_audio.py
@@ -1,7 +1,17 @@
 from paddlespeech.cli.asr.infer import ASRExecutor
+from paddlenlp import Taskflow
+
+schema = ["时间", "出发地", "目的地", "费用"]
+ie = Taskflow("information_extraction",
+              schema=schema, task_path="./audio_model/")
+asr = ASRExecutor()
+
+
+def audio_ie(audio_path):
+    asr_result = asr(audio_file=audio_path, force_yes=True)
+    ie_result = ie(asr_result)
+    return ie_result
+
 
 audio = "./data_set/test_audio.wav"
-asr = ASRExecutor()
-result = asr(audio_file=audio, model='conformer_online_wenetspeech')
-
-print(result)
+print(audio_ie(audio))
